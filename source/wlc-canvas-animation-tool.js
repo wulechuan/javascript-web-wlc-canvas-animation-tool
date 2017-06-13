bb432e461a40c0b73aab64b6267bcddde58fe9b7
吴乐川 <wulechuan@live.com>
2017-06-13 10:56:24 +0800

    requestAnimationFrame prefixes

--- source/wlc-canvas-animation-tool.js
+++ source/wlc-canvas-animation-tool.js
@@ -187,8 +187,14 @@
 
 
 
-		var requestAnimationFrame = window.requestAnimationFrame;
-
+		var requestAnimationFrame =
+			window.requestAnimationFrame
+			|| window.mozRequestAnimationFrame
+			|| window.webkitRequestAnimationFrame
+			|| window.msRequestAnimationFrame
+			|| function (callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
 
 		// Being public means being writable.
 		var publicState = _definePublicState.call(thisInstance);
